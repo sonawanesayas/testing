a23a7f9b5bf996a3490f05287349e24436901d0d
sayas sonawane <sonawanesayas@gmail.com>
2023-01-20 12:37:31 +0530

    first commit

---
+++ BubbleSort.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the alphabets: ");
+        String input = sc.nextLine();
+        // System.out.println("something something");
+        char[] alpha = input.toCharArray();
+        char temp;
+
+        for(int i = 0; i< alpha.length; i++){
+            for(int j = i + 1; j < alpha.length; j++){
+                if(alpha[i] > alpha[j]){
+                    temp = alpha[i];
+                alpha[i] = alpha[j];
+                alpha[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(new String(alpha));
+        sc.close();
+        long end = System.currentTimeMillis();
+        System.out.println("Time Taken By Program is" + (end-start));
+    }
+}
+
+
+// hello 
